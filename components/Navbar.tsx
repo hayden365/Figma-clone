f71df0cc778d6102c2cc555d3e8f618d822698e7
hayden365 <ysmh100@gmail.com>
2024-02-14 21:51:43 +0900

    feat: canvas

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+import { NavbarProps } from "@/types/type";
+import Image from "next/image";
+import ActiveUsers from "./users/ActiveUsers";
+import { memo } from "react";
+
+const Navbar = ({ activeElement }: NavbarProps) => {
+  return (
+    <nav className='flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white'>
+      <Image src='/assets/logo.svg' alt='FigPro Logo' width={58} height={20} />
+      <ActiveUsers />
+    </nav>
+  );
 };
 
-export default Navbar;
+export default memo(
+  Navbar,
+  (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement
+);
